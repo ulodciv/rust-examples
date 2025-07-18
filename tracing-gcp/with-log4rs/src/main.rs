9da4651b32e5549517481fccbc98284817be2939
Ludovic VAUGEOIS PEPIN <ludovic.vaugeois@lumapps.com>
2025-07-18 14:00:05 +0000

    Add examples for tracing vs log4rs

---
+++ tracing-gcp/with-log4rs/src/main.rs
@@ -0,0 +1,73 @@
+use chrono::{SecondsFormat, Utc};
+use log::{LevelFilter, info};
+use log4rs::append::console::{ConsoleAppender, Target};
+use log4rs::config::{Appender, Config, Root};
+use serde::Serialize;
+use tokio::task_local;
+
+task_local! {
+    pub static TASK_LOCAL_TRACE_ID: Option<String>;
+}
+
+#[derive(Clone, Eq, PartialEq, Hash, Debug, Default)]
+struct GcpJsonEncoder {
+    pub gcp_project_id: String,
+}
+
+impl log4rs::encode::Encode for GcpJsonEncoder {
+    fn encode(
+        &self,
+        w: &mut dyn log4rs::encode::Write,
+        record: &log::Record,
+    ) -> anyhow::Result<()> {
+        #[derive(Serialize)]
+        struct LogEntry {
+            severity: String,
+            message: String,
+            time: String,
+            #[serde(rename = "logging.googleapis.com/trace")]
+            #[serde(skip_serializing_if = "Option::is_none")]
+            trace: Option<String>,
+        }
+        let severity = record.level().as_str().to_lowercase();
+        let time = Utc::now().to_rfc3339_opts(SecondsFormat::Millis, true);
+        let trace_id = TASK_LOCAL_TRACE_ID.try_with(|c| c.clone()).ok().flatten();
+        let trace =
+            trace_id.map(|t| format!("projects/{}/traces/{t}", self.gcp_project_id));
+        let message = format!("{}", record.args());
+        let entry = LogEntry { severity, message, time, trace };
+        w.write_all(&serde_json::to_vec(&entry).unwrap())?;
+        w.write_all("\n".as_bytes())?;
+        Ok(())
+    }
+}
+
+fn get_gcp_project_id() -> String {
+    "PROJECT_ID_123".into()
+}
+
+async fn init_logging() {
+    let stderr = ConsoleAppender::builder()
+        .target(Target::Stderr)
+        .encoder(Box::new(GcpJsonEncoder { gcp_project_id: get_gcp_project_id() }))
+        .build();
+    let config = Config::builder()
+        .appender(Appender::builder().build("stderr", Box::new(stderr)))
+        .build(Root::builder().appender("stderr").build(LevelFilter::Info))
+        .unwrap();
+    log4rs::init_config(config).unwrap();
+}
+
+async fn do_something() {
+    info!("Doing something");
+    println!("...");
+    info!("Done doing something");
+}
+
+#[tokio::main]
+async fn main() {
+    init_logging().await;
+    info!("App started");
+    TASK_LOCAL_TRACE_ID.scope(Some("TRACE_ID_456".into()), do_something()).await;
+    info!("Exiting app");
+}
---
+++ tracing-gcp/with-tracing/src/main.rs
@@ -0,0 +1,112 @@
+use chrono::{SecondsFormat, Utc};
+use serde::Serialize;
+use serde_json::to_string;
+use tracing::field::{Field, Visit};
+use tracing::span::{Attributes, Id};
+use tracing::{Event, Instrument, Subscriber, info, info_span};
+use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::layer::Context;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::registry::LookupSpan;
+use tracing_subscriber::{Layer, registry};
+
+struct TraceId(String);
+
+#[derive(Default)]
+struct TraceIdVisitor {
+    trace_id: Option<String>,
+}
+
+impl Visit for TraceIdVisitor {
+    fn record_debug(&mut self, field: &Field, value: &dyn std::fmt::Debug) {
+        if field.name() == "trace_id" {
+            self.trace_id = Some(format!("{value:?}"))
+        }
+    }
+}
+
+#[derive(Default)]
+struct EventVisitor {
+    message: Option<String>,
+}
+
+impl Visit for EventVisitor {
+    fn record_debug(&mut self, field: &Field, value: &dyn std::fmt::Debug) {
+        if field.name() == "message" {
+            self.message = Some(format!("{value:?}"))
+        }
+    }
+}
+
+struct GcpLayer {
+    pub gcp_project_id: String,
+}
+
+impl<S> Layer<S> for GcpLayer
+where
+    S: Subscriber + for<'lookup> LookupSpan<'lookup>,
+{
+    fn on_new_span(&self, attrs: &Attributes<'_>, id: &Id, ctx: Context<'_, S>) {
+        let Some(span) = ctx.span(id) else {
+            return;
+        };
+        let mut visitor = TraceIdVisitor::default();
+        attrs.record(&mut visitor);
+        if let Some(trace_id) = visitor.trace_id {
+            span.extensions_mut().insert(TraceId(trace_id));
+        }
+    }
+
+    fn on_event(&self, event: &Event<'_>, ctx: Context<'_, S>) {
+        #[derive(Serialize)]
+        struct LogEntry {
+            severity: String,
+            message: String,
+            time: String,
+            #[serde(rename = "logging.googleapis.com/trace")]
+            #[serde(skip_serializing_if = "Option::is_none")]
+            trace: Option<String>,
+        }
+        let mut visitor = EventVisitor::default();
+        event.record(&mut visitor);
+        let message = visitor.message.unwrap_or_default();
+        let severity = event.metadata().level().as_str().to_lowercase();
+        let time = Utc::now().to_rfc3339_opts(SecondsFormat::Millis, true);
+        let mut trace = None;
+        if let Some(scope) = ctx.event_scope(event) {
+            for span in scope.from_root() {
+                let extensions = span.extensions();
+                let Some(trace_id) = extensions.get::<TraceId>() else { continue };
+                let t = &trace_id.0;
+                trace = Some(format!("projects/{}/traces/{t}", self.gcp_project_id));
+                break;
+            }
+        }
+        eprintln!("{}", to_string(&LogEntry { severity, message, time, trace }).unwrap());
+    }
+}
+
+fn get_gcp_project_id() -> String {
+    "PROJECT_ID_123".into()
+}
+
+async fn init_logging() {
+    let layer = GcpLayer { gcp_project_id: get_gcp_project_id() };
+    registry().with(layer.with_filter(LevelFilter::INFO)).init();
+}
+
+async fn do_something() {
+    info!("Doing something");
+    println!("...");
+    info!("Done doing something");
+}
+
+#[tokio::main]
+async fn main() {
+    init_logging().await;
+    info!("App started");
+    let trace_id = "TRACE_ID_456".to_string();
+    let span = info_span!("trace_id", %trace_id);
+    do_something().instrument(span).await;
+    info!("Exiting app");
+}
